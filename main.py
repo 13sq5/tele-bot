11247102ef42ef2b0c927066fe9aa57f7486d9a2
13sq5 <167219746+13sq5@users.noreply.github.com>
2025-07-06 22:11:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+ADMIN_CHAT_ID = 1033014201  # هذا رقم شاتك الخاص
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
@@ -16,11 +18,15 @@ def handle_message(msg):
     text = msg.text
     if text == '📩 SMS':
         bot.send_message(msg.chat.id, "تم استلام طلب SMS ✅")
+        bot.send_message(ADMIN_CHAT_ID, f"عميل @{msg.from_user.username} طلب خدمة SMS")
     elif text == '👥 جهات الاتصال':
         bot.send_message(msg.chat.id, "تم استلام طلب جهات الاتصال ✅")
+        bot.send_message(ADMIN_CHAT_ID, f"عميل @{msg.from_user.username} طلب خدمة جهات الاتصال")
     elif text == '📞 سجل المكالمات':
         bot.send_message(msg.chat.id, "تم استلام طلب سجل المكالمات ✅")
+        bot.send_message(ADMIN_CHAT_ID, f"عميل @{msg.from_user.username} طلب خدمة سجل المكالمات")
     else:
         bot.send_message(msg.chat.id, "اختر من الأزرار فقط 🙏")
+        bot.send_message(ADMIN_CHAT_ID, f"رسالة من @{msg.from_user.username}: {text}")
 
 bot.infinity_polling()
